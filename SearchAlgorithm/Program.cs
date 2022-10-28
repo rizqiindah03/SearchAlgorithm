a5967009d99b2dc6459b7d2bd312dce6afbe216f
rizqiindah03 <rizqiindah32@gmail.com>
2022-10-28 16:04:02 +0700

    Add project files.

---
+++ SearchAlgorithm/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[28];
+        //Number of elements in the array
+        int a;
+        //get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((new > 0)) $$ (new <= 20))
+                        break;
+                else 
+                    Console.WriteLine(
+                        )
+
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
