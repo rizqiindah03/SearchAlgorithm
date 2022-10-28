254904401b2cd3b9a2fd1f9048737faa3828fff0
rizqiindah03 <rizqiindah32@gmail.com>
2022-10-28 16:13:57 +0700

    membuat method input

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -9,9 +9,9 @@ namespace SearchAlgorithm
     class Program
     {
         //Array to be searched
-        int[] arr = new int[28];
+        int[] arr = new int[20];
         //Number of elements in the array
-        int a;
+        int n;
         //get the number of elements to store in the array
         int i;
 
@@ -22,13 +22,12 @@ namespace SearchAlgorithm
                 Console.Write("Enter the number of elements in the array");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
-                if ((new > 0)) $$ (new <= 20))
-                        break;
-                else 
-                    Console.WriteLine(
-                        )
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 elements.\n");         
 
-            }
+            
         }
         static void Main(string[] args)
         {
