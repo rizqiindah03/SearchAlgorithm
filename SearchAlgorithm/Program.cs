522392666274d400a5458921463e60dba48d3a89
rizqiindah03 <rizqiindah32@gmail.com>
2022-10-28 16:49:25 +0700

    membuat method linear search

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -80,6 +80,33 @@ namespace SearchAlgorithm
                         
                 } while ((ch == 'y') || (ch == 'y')) ;
             }
+        public void LinearSearch()
+        {
+            char ch;
+            //search for number of comparision
+            int ctr;
+            do
+            {
+                //accept the number to be searched
+                Console.Write("\nEnter the element you want to search: ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + "found add position" + (i + 1).ToString());
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array");
+                Console.WriteLine("\nNumber of comparision: " + ctr);
+                Console.Write("\nContinue search (y/n) :");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
         }
     }
 
